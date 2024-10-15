71ac8cbfa593b3edb06911c63e58adb8505bd92b
Ryan <158619162+GamerboyRyan@users.noreply.github.com>
2024-10-15 02:53:45 +0200

    Update startup.lua

--- os/startup.lua
+++ os/startup.lua
@@ -1,5 +1,17 @@
 -- startup.lua
-shell.run("updater.lua")
+term.clear()
+term.setCursorPos(1, 1)
+print("Running Updater...")
+
+local success, err = pcall(function()
+    shell.run("os/updater.lua")
+end)
+
+if not success then
+    print("Error running updater: " .. err)
+end
+
+os.sleep(1)
 
 term.clear()
 term.setCursorPos(1, 1)
