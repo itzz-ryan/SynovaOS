741cf52c31a585bb9df1c6cbc47475ef47949bd0
Ryan <158619162+GamerboyRyan@users.noreply.github.com>
2024-10-17 01:45:33 +0200

    Update startup.lua

--- os/startup.lua
+++ os/startup.lua
@@ -29,7 +29,9 @@ print("Starting Mindows...")
 
 os.sleep(2)
 
-local displayNFPModule = require("os/functions/displayNFP")
+shell.run("os/functions/displayNFP")
+
+local displayNFPModule = require("os/functions/displayNFP.lua")
 
 displayNFPModule.displayNFP("os/assets/loading.nfp")
 
