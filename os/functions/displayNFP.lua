d97fffee3c0520c171644e6d5fd5a52baeca42c6
Ryan <158619162+GamerboyRyan@users.noreply.github.com>
2024-10-17 01:16:16 +0200

    Update displayNFP.lua

--- os/functions/displayNFP.lua
+++ os/functions/displayNFP.lua
@@ -1,1 +1,12 @@
+local function displayNFP(filePath)
+    if fs.exists(filePath) then
+        term.clear()
+        term.setCursorPos(1, 1)
 
+        paintutils.drawImage(paintutils.loadImage(filePath), 1, 1)
+    else
+        print("File not found: " .. filePath)
+    end
+end
+
+return displayNFP
