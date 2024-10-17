f9660bc3716b82f90229011554a864c76d1fa626
Ryan <158619162+GamerboyRyan@users.noreply.github.com>
2024-10-17 03:14:14 +0200

    Update loading.lua

--- os/loading.lua
+++ os/loading.lua
@@ -4,9 +4,6 @@ local function displayLoadingBarScreen(message, totalSteps)
     local centerY = math.ceil(termHeight / 2)
     local barWidth = termWidth - 4
 
-    term.clear()
-    term.setCursorPos(1, 1)
-
     term.setCursorPos(centerX - #message / 2, centerY - 1)
     term.write(message)
 
@@ -19,9 +16,6 @@ local function displayLoadingBarScreen(message, totalSteps)
         term.write("[" .. string.rep("=", progress) .. string.rep(" ", barWidth - progress) .. "]")
         sleep(math.random(5, 30) / 100)
     end
-
-    term.clear()
-    term.setCursorPos(1, 1)
 end
 
 displayLoadingBarScreen("Loading, please wait...", 30)
